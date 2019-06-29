17c079b2b94b814bdd6eb86ab44b17733f095fc5
jorgemoreno <6826544+Jorg3Moreno@users.noreply.github.com>
2019-06-28 21:18:52 -0500

    Added soma changes in order to start with redux

---
+++ src/components/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class App extends Component {
+  state = { users: [] };
+
+  async componentDidMount() {
+    const req = await axios.get("https://jsonplaceholder.typicode.com/users");
+    this.setState({
+      users: req.data
+    });
+  }
+
+  ponerFilas = () =>
+    this.state.users.map(user => {
+      return (
+        <tr key={user.id}>
+          <td>{user.name}</td>
+          <td>{user.email}</td>
+          <td>{user.website}</td>
+        </tr>
+      );
+    });
+
+  render() {
+    return (
+      <div className="margen">
+        <table className="tabla">
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Correo</th>
+              <th>Enlace</th>
+            </tr>
+          </thead>
+          <tbody>{this.ponerFilas()}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default App;
