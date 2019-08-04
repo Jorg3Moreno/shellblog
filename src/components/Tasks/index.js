d0e90a1d42ddca80e70e03b5fe0517f367ffabca
jorgemoreno <6826544+Jorg3Moreno@users.noreply.github.com>
2019-08-03 22:21:39 -0500

    Connected actions to component

--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
+import * as taskActions from "../../actions/tasksActions";
+
 class Tasks extends Component {
+  componentDidMount() {
+    this.props.getAll();
+  }
+
   render() {
     console.log(this.props);
     return <div>Tasks</div>;
   }
 }
 
-const mapStateToProps = ({ taskReducer }) => taskReducer;
+const mapStateToProps = ({ tasksReducer }) => tasksReducer;
 
-export default connect(mapStateToProps)(Tasks);
+export default connect(
+  mapStateToProps,
+  taskActions
+)(Tasks);
