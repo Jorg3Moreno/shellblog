6af4bd2a7f4562b0c5b01f115d9a5c072e8bce51
jorgemoreno <6826544+Jorg3Moreno@users.noreply.github.com>
2019-08-04 16:24:48 -0500

    Displaying todos

--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -2,15 +2,50 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 import * as taskActions from "../../actions/tasksActions";
+import Spinner from "../spinner";
+import Fatal from "../Fatal";
 
 class Tasks extends Component {
   componentDidMount() {
     this.props.getAll();
   }
 
+  putTasks = user_id => {
+    const { tasks } = this.props;
+    const byUser = {
+      ...tasks[user_id]
+    };
+
+    return Object.keys(byUser).map(taskId => (
+      <div key={taskId}>
+        <input type="checkbox" defaultChecked={byUser[taskId].completed} />
+        {byUser[taskId].title}
+      </div>
+    ));
+  };
+
+  showContent = () => {
+    const { tasks, loading, error } = this.props;
+
+    if (loading) {
+      return <Spinner />;
+    }
+
+    if (error) {
+      return <Fatal message={this.props.error} />;
+    }
+
+    return Object.keys(tasks).map(user_id => (
+      <div key={user_id}>
+        <h2>User {user_id}</h2>
+        <div className="task_container">{this.putTasks(user_id)}</div>
+      </div>
+    ));
+  };
+
   render() {
     console.log(this.props);
-    return <div>Tasks</div>;
+    return <div>{this.showContent()}</div>;
   }
 }
 
