e9c0eba449be30535b9d32cbd9cef81eef0ca2e8
jorgemoreno <6826544+Jorg3Moreno@users.noreply.github.com>
2019-08-03 21:05:36 -0500

    Added reducer and types for Task component

---
+++ src/reducers/tasksReducer.js
@@ -0,0 +1,22 @@
+import { GET_ALL, LOADING, ERROR } from "./../types/taskTypes";
+
+const INITIAL_STATE = {
+  tasks: [],
+  loading: false,
+  error: null
+};
+
+const reducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case GET_ALL:
+      return { ...state, tasks: action.payload, loading: false, error: null };
+    case LOADING:
+      return { ...state, loading: true };
+    case ERROR:
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
---
+++ src/types/taskTypes.js
@@ -0,0 +1,3 @@
+export const GET_ALL = "get_users";
+export const LOADING = "tasks_loading";
+export const ERROR = "tasks_error";
